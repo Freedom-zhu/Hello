b428d6a46a54d83d3fad7df747fdfe5f0b244eb2
Freedom-zhu <114126978+Freedom-zhu@users.noreply.github.com>
2023-01-07 16:54:02 +0800

    定义电子日历
    学习了面向对象技术，学会了通过封装属性（变量）和行为（函数）定义类，现在他要设计一个电子日历类TDate。
    它有3个私有数据成员：Month,Day,Year和若干个公有成员函数，要求：
    （1）带有默认形参值的构造函数，默认值为0, 0, 0；
    （2）输出日期函数，用“日/月/年”格式输出日期；
    （3）设置日期函数，从键盘输入年、月、日。

---
+++ nowcoder4.c
@@ -0,0 +1,60 @@
+#define _CRT_SECURE_NO_WARNINGS
+//定义电子日历
+//输入描述：一行，三个整数，用空格分隔，分别表示年、月、日。
+//输出描述：一行，用“日/月/年”格式输出日期。
+#include<stdio.h>
+
+typedef struct day_t Pday_t;
+typedef void (*TInitDay)(Pday_t* day);
+typedef void (*TSetDay)(Pday_t* day, int y, int m, int d);
+typedef void (*TShowDay)(Pday_t* day);
+
+struct day_t //Pday-t
+{
+    int Day;
+    int Month;
+    int Year;
+    TInitDay init;
+    TSetDay  set;
+    TShowDay show;
+};
+
+void day_init(Pday_t* day)//TInitDay   初始化
+{
+    day->Year = 0;
+    day->Day = 0;
+    day->Month = 0;
+}
+
+void day_set(Pday_t* day, int y, int m, int d)//TSetDay   设置
+{
+    day->Year = y;
+    day->Month = m;
+    day->Day = d;
+}
+
+void day_show(Pday_t* day) //TShowDay   打印
+{
+    printf("%d/%d/%d", day->Day, day->Month, day->Year);
+}
+
+Pday_t M_day = 
+{
+    .init = day_init,
+    .set = day_set,
+    .show = day_show,
+};
+
+int main() {
+    int y, m, d;
+    scanf("%d", &y);
+    scanf("%d", &m);
+    scanf("%d", &d);
+
+    M_day.init(&M_day);
+    M_day.set(&M_day, y, m, d);
+    M_day.show(&M_day);
+
+    return 0;
+}
+
